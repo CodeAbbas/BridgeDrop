19b88f47225fb6d5cd7ff220119078996d32c553
Abbas Uddin <105127468+CodeAbbas@users.noreply.github.com>
2026-02-24 13:04:42 +0000

    Add TURN server configuration to rtcConfig

--- src/components/BridgeDrop.tsx
+++ src/components/BridgeDrop.tsx
@@ -16,6 +16,11 @@ import {
 const rtcConfig = {
   iceServers: [
     { urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'] },
+    { 
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
   ],
 };
 
